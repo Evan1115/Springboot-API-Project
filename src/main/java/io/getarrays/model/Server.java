66a1b407bfae93d6605443f5098b474de65798de
Joram <jing111597@gmail.com>
2022-04-11 21:59:05 +0800

    initial file structure

---
+++ src/main/java/io/getarrays/model/Server.java
@@ -0,0 +1,9 @@
+package io.getarrays.model;
+
+public class Server {
+    private Long id;
+    private String ipAddress;
+    private String name;
+    private String memory;
+    private String type;
+}
