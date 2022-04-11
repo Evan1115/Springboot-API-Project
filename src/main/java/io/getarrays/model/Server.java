b40f25dc414beab651bd5ccb9e68f3e62f7c7046
Joram <jing111597@gmail.com>
2022-04-11 23:00:45 +0800

    created enum, model, interfaces for repository and services

--- src/main/java/io/getarrays/model/Server.java
+++
@@ -1,9 +0,0 @@
-package io.getarrays.model;
-
-public class Server {
-    private Long id;
-    private String ipAddress;
-    private String name;
-    private String memory;
-    private String type;
-}
